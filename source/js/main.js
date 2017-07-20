df6d25d082871fe8c29638fd10b70450d0c24704
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-07-20 11:54:42 +0200

    adding and removing into arrays - object

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,8 @@
+var data = {
+    toDo: [],
+    completed: []
+};
+
 var doneGlyph = '<span id="done" class="glyphicon glyphicon-ok-circle" aria-hidden="true"></span>';
 var removeGlyph = '<span id="remove" class="glyphicon glyphicon-remove-circle" aria-hidden="true"></span>';
 
@@ -7,6 +12,8 @@ document.getElementById('addItem').addEventListener('click', function () {
     if (value) {
         addItemToDo(value);
         document.getElementById('item').value = '';
+
+        data.toDo.push(value);
     }
 
 })
@@ -14,6 +21,16 @@ document.getElementById('addItem').addEventListener('click', function () {
 function removeItem() {
     var item = this.parentNode.parentNode;
     var parent = item.parentNode;
+    var parentID = parent.id;
+    var value = item.innerText;
+
+    if (parentID === 'toDo') {
+        data.toDo.splice(data.toDo.indexOf(value), 1);
+    } else {
+        data.completed.splice(data.completed.indexOf(value), 1);
+    }
+
+    console.log(data);
 
     parent.removeChild(item);
 }
@@ -22,8 +39,19 @@ function doneItem() {
     var item = this.parentNode.parentNode;
     var parent = item.parentNode;
     var parentID = parent.id;
+    var value = item.innerText;
 
-    var target = (parentID === 'toDo') ? document.getElementById('completed'):document.getElementById('toDo');
+    if (parentID === 'toDo') {
+        data.toDo.splice(data.toDo.indexOf(value), 1);
+        data.completed.push(value);
+    } else {
+        data.completed.splice(data.completed.indexOf(value), 1);
+        data.toDo.push(value);
+    }
+
+    console.log(data);
+
+    var target = (parentID === 'toDo') ? document.getElementById('completed') : document.getElementById('toDo');
 
     parent.removeChild(item);
     target.insertBefore(item, target.childNodes[0]);
